672a324e66c92f52b50956359fc5c5c1e1c54bbd
Jayani-C <jayaninew1999@gmail.com>
2024-09-11 11:29:45 +0530

    0.0.28

--- components/custom/StockOutElecModal.tsx
+++ components/custom/StockOutElecModal.tsx
@@ -103,7 +103,7 @@ const StockAddModal: FC<StockAddModalProps> = ({ id, isOpen, setIsOpen }) => {
     return (
 		<Modal isOpen={isOpen} setIsOpen={setIsOpen} size='xl' titleId={id}>
 			<ModalHeader setIsOpen={setIsOpen} className='p-4'>
-				<ModalTitle id="">{'Stock Outaaaaaaa'}</ModalTitle>
+				<ModalTitle id="">{'Stock Out'}</ModalTitle>
 			</ModalHeader>
 			<ModalBody className='px-4'>
 				<div className='row g-4'>
